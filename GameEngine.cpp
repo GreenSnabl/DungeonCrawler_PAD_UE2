957a8308878d0db4b7f7f9fb61a9ae21a8fc3f4b
GreenSnabl <rubensbarr@web.de>
2018-05-03 20:40:20 +0200

    fixed multiple print issue

--- GameEngine.cpp
+++ GameEngine.cpp
@@ -25,78 +25,73 @@ using std::string;
 using std::vector;
 
 
-GameEngine::GameEngine(int height, int width, const std::string& data) : m_map{new DungeonMap(height, width, data)} 
+GameEngine::GameEngine(int height, int width, const std::string& data) : m_map{new DungeonMap(height, width, data)}
 {
     m_charVec.push_back(new Player);
-    m_map->place({5,11}, m_charVec[0]);
+    m_map->place({5, 11}, m_charVec[0]);
 
 }
 
-    GameEngine::GameEngine(const std::string& mapFile, const std::string& connectorFile)
-    {
-        vector<string> vecMap;
-        if(!loadMap(mapFile, vecMap)) throw;
-        
-        m_map = new DungeonMap(vecMap.size(), vecMap[0].size(), vecMap);
-        
-        loadEntities(vecMap);
-        
-        vector<Position> vecPos;        
-        if (!loadConnectors(connectorFile, vecPos)) throw;
-        for (int i = 0; i < vecPos.size() - 1; i = i + 2) {
-            dynamic_cast<Active*>(m_map->find(vecPos[i]))->registerPassive(dynamic_cast<Passive*>(m_map->find(vecPos[i+1])));
-        }
-        
-    }
-    
-    bool GameEngine::loadMap(const string& mapFile, vector<string>& vecMap)
-    {
-        string str = "";
-        std::ifstream ifs;
-        
-        ifs.open(mapFile);
-        while (getline(ifs, str)) {
-            vecMap.push_back(str);
-        }
-        ifs.close();
-        
-        return vecMap.size() > 0 && vecMap[0].size() > 0 && (vecMap[0].size() == vecMap[vecMap.size() - 1].size());
-    }
-    
-    bool GameEngine::loadConnectors(const std::string& connectorFile, std::vector<Position>& vecPos)
-    {
-        Position tempPos;
-        std::stringstream ss;
-        string tempStr;
-        std::ifstream ifs;
-        ifs.open(connectorFile);
-         
-        while (getline(ifs, tempStr)) {
-            ss.str(tempStr);
-            while(ss >> tempPos.y >> tempPos.x){
-                vecPos.push_back(tempPos);   
-            }
-            ss.clear();
-        }
-        
-        ifs.close();
-        return vecPos.size() % 2 == 0;
+GameEngine::GameEngine(const std::string& mapFile, const std::string& connectorFile) {
+    vector<string> vecMap;
+    if (!loadMap(mapFile, vecMap)) throw;
+
+    m_map = new DungeonMap(vecMap.size(), vecMap[0].size(), vecMap);
+
+    loadEntities(vecMap);
+
+    vector<Position> vecPos;
+    if (!loadConnectors(connectorFile, vecPos)) throw;
+    for (int i = 0; i < vecPos.size() - 1; i = i + 2) {
+        dynamic_cast<Active*> (m_map->find(vecPos[i]))->registerPassive(dynamic_cast<Passive*> (m_map->find(vecPos[i + 1])));
     }
 
-    void GameEngine::loadEntities(const vector<string>& vecMap)
-    {
-        Character* temp = nullptr;
-        for (int i = 0; i < vecMap.size(); ++i) {
-            for (int j = 0; j < vecMap[i].size(); ++j) {
-                if (temp = Character::makeCharacter(vecMap[i][j]))
-                {
-                    m_charVec.push_back(temp);
-                    m_map->place({j,i}, m_charVec[m_charVec.size()-1]);
-                    temp = nullptr;
-                }
+}
+
+bool GameEngine::loadMap(const string& mapFile, vector<string>& vecMap) {
+    string str = "";
+    std::ifstream ifs;
+
+    ifs.open(mapFile);
+    while (getline(ifs, str)) {
+        vecMap.push_back(str);
+    }
+    ifs.close();
+
+    return vecMap.size() > 0 && vecMap[0].size() > 0 && (vecMap[0].size() == vecMap[vecMap.size() - 1].size());
+}
+
+bool GameEngine::loadConnectors(const std::string& connectorFile, std::vector<Position>& vecPos) {
+    Position tempPos;
+    std::stringstream ss;
+    string tempStr;
+    std::ifstream ifs;
+    ifs.open(connectorFile);
+
+    while (getline(ifs, tempStr)) {
+        ss.str(tempStr);
+        while (ss >> tempPos.y >> tempPos.x) {
+            vecPos.push_back(tempPos);
+        }
+        ss.clear();
+    }
+
+    ifs.close();
+    return vecPos.size() % 2 == 0;
+}
+
+void GameEngine::loadEntities(const vector<string>& vecMap) {
+    Character* temp = nullptr;
+    for (int i = 0; i < vecMap.size(); ++i) {
+        for (int j = 0; j < vecMap[i].size(); ++j) {
+            if (temp = Character::makeCharacter(vecMap[i][j])) {
+                m_charVec.push_back(temp);
+                m_map->place({j, i}, m_charVec[m_charVec.size() - 1]);
+                temp = nullptr;
             }
         }
     }
+}
 
 GameEngine::GameEngine(const GameEngine& orig) {
 }
@@ -108,26 +103,23 @@ GameEngine::~GameEngine() {
     }
 }
 
-bool GameEngine::turn()
-{
+bool GameEngine::turn() {
     for (int i = 0; i < m_charVec.size(); ++i) {
         Position charPos = m_map->find(m_charVec[i]);
         Position movement = intToPos(m_charVec[i]->move());
         m_map->find(charPos)->onLeave(m_map->find({charPos.x + movement.x, charPos.y + movement.y}));
-        m_map->print();
+
     }
+    m_map->print();
 }
 
-bool GameEngine::finished()
-{
+bool GameEngine::finished() {
     return rounds > 20;
 }
 
-void GameEngine::run()
-{
+void GameEngine::run() {
     m_map->print();
-    while (!finished())
-    {
+    while (!finished()) {
         turn();
         ++rounds;
     }
@@ -135,17 +127,16 @@ void GameEngine::run()
 
 int GameEngine::rounds = 0;
 
-Position intToPos(int dir)
-{
+Position intToPos(int dir) {
     switch (dir) {
-        case 1 : return {-1, 1};
-        case 2 : return { 0, 1};
-        case 3 : return { 1, 1};
-        case 4 : return {-1, 0};
-        case 5 : return { 0, 0};
-        case 6 : return { 1, 0};
-        case 7 : return {-1, -1};
-        case 8 : return { 0, -1};
-        case 9 : return { 1, -1};
+        case 1: return {-1, 1};
+        case 2: return { 0, 1};
+        case 3: return { 1, 1};
+        case 4: return {-1, 0};
+        case 5: return { 0, 0};
+        case 6: return { 1, 0};
+        case 7: return {-1, -1};
+        case 8: return { 0, -1};
+        case 9: return { 1, -1};
     }
 }
